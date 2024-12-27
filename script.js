dd3358c6f9bbba46219342b31bd3d0d432f19404
José Isaac <joseisaacnascimento@gmail.com>
2024-12-27 16:05:54 -0300

    Adiciona hora e imagem de fundo.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log(window.Date());
+function atualizarRelogio() {
+  // relogio / h2
+  let relogio = document.getElementById("relogio");
+  //   data
+  let dataAtual = new Date();
+  //   formatar a data
+  let opcoes = {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  };
+  //   data formatada
+  let horaFormatada = dataAtual.toLocaleTimeString("pt-BR", opcoes);
+
+  relogio.textContent = "São exatamente " + horaFormatada;
+}
+//   chama a função
+atualizarRelogio();
+// Atualiza o relógio a cada segundo
+setInterval(atualizarRelogio, 1000);
