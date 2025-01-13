d25d9b96dba47f392991c055fd105709fe1035eb
José Isaac <joseisaacnascimento@gmail.com>
2025-01-13 15:35:22 -0300

    Mudanças no layout e design.

--- README.md
+++
@@ -1,9 +0,0 @@
-# Relógio em Tempo Real
-
-Este é um projeto simples de um relógio que exibe a hora atual em tempo real, atualizado a cada segundo.
-
-## Tecnologias Utilizadas
-
-- HTML
-- CSS
-- JavaScript
--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+// h1
+let titulo = document.querySelector("#box h1");
+let mensagem = document.querySelector("#box h2");
 function atualizarRelogio() {
-  // relogio / h2
-  let relogio = document.getElementById("relogio");
-  //   data
-  let dataAtual = new Date();
-  //   formatar a data
-  let opcoes = {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  };
-  //   data formatada
-  let horaFormatada = dataAtual.toLocaleTimeString("pt-BR", opcoes);
-
-  relogio.textContent = "São exatamente " + horaFormatada;
+  // cria novo horario
+  let novaData = new Date();
+  // quebra data em partes
+  let horas = String(novaData.getHours()).padStart(2, "0");
+  let minutos = String(novaData.getMinutes()).padStart(2, "0");
+  let segundos = String(novaData.getSeconds()).padStart(2, "0");
+  // titulo recebe hora atual
+  titulo.innerHTML = `${horas}:${minutos}:${segundos}`;
 }
-//   chama a função
+// chama funcao
 atualizarRelogio();
-// Atualiza o relógio a cada segundo
+
+// funcao de mensagem
+function saudacao() {
+  let body = document.querySelector("body");
+  let dataNova = new Date();
+  let hora = dataNova.getHours();
+  // logica da saudacao
+  if (hora >= 5 && hora < 12) {
+    mensagem.innerHTML = "Bom dia";
+  } else if (hora >= 12 && hora < 18) {
+    mensagem.innerHTML = "Boa tarde";
+  } else {
+    mensagem.innerHTML = "Boa noite";
+  }
+}
+// funcao se chama a cada 1s
 setInterval(atualizarRelogio, 1000);
+saudacao();
